75ecd46100cfd4dbdd77a31869273f6907663521
DaitheA <alex_d.4@mail.ru>
2016-04-27 20:23:40 +0600

    jurnal

--- database/migrations/2016_04_26_150226_create_jurnal_table.php
+++ database/migrations/2016_04_26_150226_create_jurnal_table.php
@@ -12,7 +12,16 @@ class CreateJurnalTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('jurnal', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('student_id');
+            $table->integer('subject_id');
+            $table->dateTime('date');
+            $table->integer('class_works');
+            $table->integer('home_works');
+            $table->integer('visits');
+            $table->string('comment');
+        });
     }
 
     /**
